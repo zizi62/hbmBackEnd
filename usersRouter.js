367a29fac21a7bd869f714ce74f5728b46518fc8
Zoia <zizi2405@gmail.com>
2020-07-30 17:13:35 -0400

    change response

--- usersRouter.js
+++ usersRouter.js
@@ -11,23 +11,17 @@ router.use(function timeLog (req, res, next) {
 })
 router.get('/', async function (req, res) {
     let users = await getUsers(req.query.search)    
-     res.send(users)
+     res.send({ success: true, users })
 })
 
 
 router.post('/', async function (req, res) {
+  
     console.log("req"+ req)
     let user = req.body.user
     let result = await addUser(user)
      res.send({ success: true, user })
 })
 
-router.put('/', async function (req, res) {
-    let firstName = req.body.firstName
-    let lastName = req.body.lastName
-    let email = req.body.email
-    // let id = req.body.id
-    await updateUser(firstName,lastName,email )
-     res.send({ success: true })
-})
+
 module.exports = router
